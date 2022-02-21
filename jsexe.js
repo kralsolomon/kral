ada7ef915c7c3b3901c1f1578899453d25a8674d
kralsolomon <solomonkein228@gmail.com>
2022-02-21 19:10:01 +0600

    assign4

---
+++ jsexe.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+
+    $('#water').ripples({
+        dropRadius: 20,
+        resolution: 200,
+        
+    });
+
+
+    function zvuk() {
+
+        document.getElementById('#sound').play();
+    
+    }
+});
