edf980266f677654c727571ce59f3fb9269e4db1
kralsolomon <solomonkein228@gmail.com>
2022-02-23 15:12:46 +0600

    one more

--- jsexe.js
+++ jsexe.js
@@ -1,16 +1,39 @@
 $(document).ready(function()
 {
 
+
+
     $('#water').ripples({
         dropRadius: 20,
         resolution: 512,
         
     });
-
-
-    function zvuk() {
-
-        document.getElementById('#sound').play();
-    
-    }
 });
+
+
+function calc(){
+    var height = document.getElementById("h").value;
+    var weight = document.getElementById("w").value;
+    var res = Math.round(weight / height**2 * 10000);
+    if(height <= 0 || weight <= 0 ){
+        alert("Impossible to calculate")
+    }
+    else if (res < 18.5) {
+        alert("Your BMI is " + res + ", compare results with table below.");
+    }
+    else if (res > 18.5 && res < 24.9){
+        alert("Your BMI is " + res + ", compare results with table below.");
+    }
+    else 
+        alert("Your BMI is " + res + ", compare results with table below.") ;
+}
+
+
+
+
+
+
+
+
+      
+      
